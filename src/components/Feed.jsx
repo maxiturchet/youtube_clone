8c95805658a8a625f49027385308123f26f8dc57
maxiturchet <71859870+maxiturchet@users.noreply.github.com>
2023-01-17 11:42:07 -0300

    Primer commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+
+const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New')
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: {xs: "column", md: "row"}}}>
+      <Box sx={{ height: { xs: 'auto', md:'92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        >
+          
+        </Sidebar> 
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2023 CloneTube Media
+        </Typography>
+      </Box>
+      <Box p={2} sx={{overflowY: 'auto', height: '909vh', flex: 2}}>
+        <Typography 
+            variant="h4"
+            fontWeight= "bold" mb={2} sx={{ color: 'white'}}
+        >
+          {selectedCategory} <span 
+          style={{ color: '#F31503'}}>
+            videos
+          </span>
+        </Typography>
+        <Videos videos={[]}/>
+      </Box>
+    </Stack>
+  )
+}
+
+export default Feed;
